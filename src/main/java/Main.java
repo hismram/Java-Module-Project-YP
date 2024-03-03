27a8e50b1367ec3c3a44dd59b4fc808f40f7f07d
rr.hismatullin <hismram@gmail.com>
2024-03-03 19:16:03 +0500

    Добавлен класс Calculator Реализовано чтение количество персон и обработка ошибок Добавлен класс товара Good Реализовано чтение товаров и обработка ошибок Реализовано форматирование (Formatter) строки валюты(рубль / рублей) Добавлен метод вывода товаров и итога

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,37 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new Calculator(initPersonsCount());
+    }
+
+    /**
+     * Получает количество персон
+     */
+    private static int initPersonsCount() {
+        Scanner scanner = new Scanner(System.in);
+        int personsCount;
+
+        System.out.println("На сколько человек нужно разделить счет?");
+        while (true) {
+            // Пытаемся прочитать число персон, если не удается выводим предупреждение и повторяем попытку
+            if (scanner.hasNextInt()) {
+                personsCount = scanner.nextInt();
+
+                // Если получили количество персон более 2х останавливаем ввод,
+                // в противном случае выводим предупреждение и повторяем попытку
+                if (personsCount > 1) {
+                    break;
+                } else {
+                    System.out.println("Число не может быть меньше 2, повторите ввод");
+                    scanner.nextLine();
+                }
+            } else {
+                System.out.println("Некорретные данные, повторите ввод");
+                scanner.nextLine();
+            }
+        }
+
+        return personsCount;
     }
 }
